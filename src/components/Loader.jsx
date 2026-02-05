719b09ce8ea1e6bf8a6a7fc439ff370f363c8b87
dipanshupandey <dipanshupandey4488@gmail.com>
2026-02-05 09:19:35 +0530

    Loader done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center text-center'>
-      <div className='border-2 border-black h-40 w-40 absolute rounded-full'></div>
-      <span>LOADING...</span>
+    <div className='min-h-screen flex justify-center items-center text-center '>
+      <div className='border-2 border-black h-40 w-40 absolute rounded-full animate-spin transition-all ease-linear duration-1000 border-t-2 border-r-2
+            border-b-0 border-l-0
+            animate-spin-color
+            '></div>
+      <span className="animate-loading 
+  font-medium
+  tracking-[0.20em]   /* 👈 tech feel */
+  text-gray-900">LOADING...</span>
     </div>
   )
 }
